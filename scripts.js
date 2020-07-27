fd3a4f46c3b2536df6fd155f601b5f1e9dc26f98
suffiang96 <56616975+suffiang96@users.noreply.github.com>
2020-07-27 14:11:19 -0700

    updates

--- scripts.js
+++ scripts.js
@@ -3,6 +3,6 @@ var map = L.map('map').setView([47.615428, -122.334145], 11);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 100,
-    id: 'mapbox.streets',
+    id: 'mapbox/streets-v11',
     accessToken:'pk.eyJ1Ijoic3VmZmlhbmc5NiIsImEiOiJjazJqb3V4cXAwbXN1M2N0cmNlMDB0ODdyIn0.bSVgjiBLodS46nfCU1cKMw'
 }).addTo(map);
