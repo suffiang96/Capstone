0b3d51a951a0d28d4400e852b29f1ab538844eed
suffiang96 <56616975+suffiang96@users.noreply.github.com>
2020-07-27 19:16:15 -0700

    updates

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-
+$.getJSON("SideChanHab1718.json",function(sideChan){
+  L.geoJson (sideChan, {
+ pane: "linePane",
+ style: function(feature){
+   return { color: 'black',
+          dashArray: '6',
+          weight: 2.5 ,
+          fillOpacity: 0.7 };
+ },
+ onEachFeature: function( feature, layer ){
+   layer.bindPopup( feature.properties.ChannelTyp + " type channel" )
+ }
+  }).addTo(map);
+});
