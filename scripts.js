825ff94d033b065ae7a84c810258347912c58c13
suffiang96 <56616975+suffiang96@users.noreply.github.com>
2020-07-28 16:10:17 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 var map = L.map('map').setView([47.207404, -121.507328], 11);
 
-
 $.getJSON("WatersEdge.geojson",function (waterEdge) {
   L.geoJson (waterEdge, {
     style: function(feature){
       return {
-        color: 'blue',
+        color: '#3399ff',
         fillOpacity: 0.5,
+        weight: 1,
       }
     }
   }).addTo(map);
@@ -16,8 +16,8 @@ $.getJSON("All_Thalweg.geojson",function (allThalweg) {
   L.geoJson (allThalweg, {
     style: function(feature){
       return {
-        color: 'green',
-        weight: 2,
+        color: '#001a33',
+        weight: 3,
         fillOpacity: 1,
       }
     }
@@ -28,7 +28,7 @@ $.getJSON("WetChanXS.geojson",function (wetChan) {
   L.geoJson (wetChan, {
     style: function(feature){
       return {
-        color: 'black',
+        color: '#400080',
         weight: 1,
         fillOpacity: 1,
       }
@@ -36,7 +36,79 @@ $.getJSON("WetChanXS.geojson",function (wetChan) {
   }).addTo(map);
 });
 
+//Style by habitat type
+$.getJSON("SideChanHab1718.geojson",function (sideChan) {
+  L.geoJson (sideChan, {
+    style: function(feature){
+      var color,
+        type = feature.properties.ChannelTyp;
+      if ( type === "D" ) color = '#990000';
+      else if ( type === 'I' ) color = '#e68a00';
+      else if ( type === 'F' ) color = '#00802b';
+      else color === '#FFFFFF';
+      return {weight: 2, color: color, fillOpacity: 1 };
 
+      },
+
+      onEachFeature: function( feature, layer ){
+        layer.bindPopup( feature.properties.ChannelTyp + " type channel" )
+      }
+  }).addTo(map);
+});
+
+//Use conditional styling to refelct size of the jam
+var defaultMark = {
+  radius: 1,
+  fillColor: "#996633",
+  color: "#000",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8,
+};
+
+$.getJSON("Jams1718.geojson",function (jams) {
+  L.geoJson (jams, {
+    pointToLayer: function (feature, latlng) {
+      var radius,
+        size = feature.properties.JamSize;
+      if ( size == "Small" ) radius = 5;
+      else if ( size == 'Medium' ) radius = 10;
+      else if ( size == 'Large' ) radius = 15;
+      else radius == 1;
+
+      return L.circleMarker(latlng, radius, defaultMark)
+  },
+
+      onEachFeature: function( feature, layer ){
+          layer.bindPopup( "<p>This is a " + feature.properties.JamSize + " sized jam</p>" + "<p>It is a " + feature.properties.JamType + " type of jam</p>")
+        }
+      }).addTo(map);
+    });
+
+//Marked Wood
+var wood = {
+  radius: 5,
+  fillColor: "#996633",
+  color: "#000",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8,
+};
+
+$.getJSON("MarkedWood1718.geojson",function (mWood) {
+  L.geoJson (mWood, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, wood )
+  },
+      onEachFeature: function( feature, layer ){
+          layer.bindPopup( "<p>This is " + feature.properties.WoodSize + " sized wood</p>")
+      }
+    }).addTo(map);
+});
+
+
+
+//Basemaps
 var layer = L.esri.basemapLayer('Topographic').addTo(map);
 var layerLabels;
 
